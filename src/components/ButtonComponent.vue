ef34e0e97e8287630492326c4061843e4203f6b8
Audrey <a.chobert@e-cobot.com>
2020-06-16 16:01:53 +0200

    début button

---
+++ src/components/ButtonComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <button v-on:click="getInfos()"> Super Bouton </button>
+        <h3>{{"Nom du bouton : " + this.nom }}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ButtonComponent",
+    props: {
+        msg: String
+    },
+    data(){
+        return{
+            object: {
+                nom: "buttonTest",
+                couleur: "red",
+            }
+        }  
+    },
+    methods: {
+        getInfos(){
+            return "Nom du bouton : " + this.nom + ". Couleur du bouton : " + this.couleur;
+        },
+    }    
+}
+</script>
--- src/views/Auth.vue
+++ src/views/Auth.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="auth">
-    <h1>Vous êtes connecté. Bienvenue</h1>
+    <h1>Vous êtes connecté.</h1>
+    <ButtonComponent/>
+
   </div>
 </template>
 
 <script>
 
+import ButtonComponent from '@/components/ButtonComponent';
 
 export default {
   name: 'Auth',
-  data(){
-    return{};
+  components:{
+    ButtonComponent,
   }
 }
 </script>
