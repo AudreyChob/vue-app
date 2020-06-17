df435951e46de216ba0f1839df0877d2393a649e
Audrey <a.chobert@e-cobot.com>
2020-06-17 16:53:05 +0200

    add pwa plugin + pwa.themeColor

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,8 @@ module.exports = {
   outputDir: 'docs',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/vue-app/'
-    : '/'
+    : '/',
+    pwa: {
+      themeColor: '#000',
+    },
 }
-
